70b83e86119fb5a9c09cb1d6268abe82dc117871
yuweihao17 <160132285+yuweihao17@users.noreply.github.com>
2026-09-20 23:27:42 +0800

    chore: bump version to v1.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.cpaphone"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 153
+        versionName = "1.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
